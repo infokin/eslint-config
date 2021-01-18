bc39317ff11d07920f76258876888dc999d21c09
Johannes Hillert <johannes.hillert@infokin.org>
2021-01-18 19:25:50 +0100

    extended tests for JavaScript and TypeScript rules

--- test/test_javascript.js
+++ test/test_javascript.js
@@ -1,21 +1,35 @@
+//@formatter:off
+
 // No console ('no-console')
 console.log('No console!');
+
+// No `var` (`no-var`)
+var one = 1;
+
+// Use const (`prefer-const`)
+let var1 = 5;
+
+// No whitespace before semicolon (`semi-spacing`)
+method1(var1) ;
+
+// Enforce whitespace after semicolon (`semi-spacing`)
+const a = 1;const b = 2;
+method1(a, b);
 
 // No dangling comma (`@typescript-eslint/comma-dangle`)
 const list = [one, 2, 3,];
 list.push(4);
 
-// No unused variables (`@typescript-eslint/no-unused-vars`)
-const tmp = 'unused';
-
-// No unused methods (`@typescript-eslint/no-unused-vars`)
+// No unused methods (warning) (`@typescript-eslint/no-unused-vars`)
 function method2() {
+    // No unused variables (warning) (`@typescript-eslint/no-unused-vars`)
+    const tmp = 'unused';
 }
 
 // Ignore arguments with underscore (`argsIgnorePattern`)
 function method1(_a1, _a2, _a3) {
+    list.push(5);
 }
-
 method1();
 
 // Use single quotes (`@typescript-eslint/quotes`)
@@ -23,5 +37,6 @@ let quotes = "double";
 quotes = 'single';
 method1(quotes);
 
-// TODO: using `var` should lead to an error
-var one = 1;
+// Missing semicolon (`@typescript-eslint/semi`)
+const myVar = 5
+method1(myVar)
--- test/test_typescript.ts
+++ test/test_typescript.ts
@@ -1,40 +1,85 @@
+//@formatter:off
+
 // No console ('no-console')
 console.log('No console!');
 
+// No `var` (`no-var`)
+var one: number = 1;
+
+// Use const (`prefer-const`)
+let var1: number = 5;
+
+// No whitespace before semicolon (`semi-spacing`)
+method1(var1) ;
+
+// Enforce whitespace after semicolon (`semi-spacing`)
+const a: number = 1;const b: number = 2;
+method1(a, b);
+
 // No dangling comma (`@typescript-eslint/comma-dangle`)
-const list = [one, 2, 3,];
+const list: number[] = [one, 2, 3,];
 list.push(4);
 
-// No unused variables (`@typescript-eslint/no-unused-vars`)
-const tmp = 'unused';
-
 // No unused methods (`@typescript-eslint/no-unused-vars`)
-function method2() {
+function method2(): void {
+    // No unused variables (`@typescript-eslint/no-unused-vars`)
+    const tmp: string = 'unused';
 }
 
 // Ignore arguments with underscore (`argsIgnorePattern`)
-function method1(_a1?, _a2?) {
+function method1(_a1?: unknown, _a2?: unknown, _a3?: unknown): void {
+    list.push(5);
 }
-
 method1();
 
+// No useless constructors (`@typescript-eslint/no-useless-constructor`)
+class A {
+    constructor() { }
+}
+const classA: A = new A();
+method1(classA);
+
 // Use single quotes (`@typescript-eslint/quotes`)
-let quotes = "double";
+let quotes: string = "double";
 quotes = 'single';
 method1(quotes);
 
-// TODO: using `var` should lead to an error
-var one = 1;
+// Missing semicolon (`@typescript-eslint/semi`)
+const myVar: number = 5
+method1(myVar)
 
-// TODO: no semicolon should lead to an error
-let myVar = 5
-method1(myVar);
-
-// TODO: no typedef should lead to an error
-const myString = 'Some string!';
-method1(myString);
-
-// TODO: no return value for methods should lead to an error
+// Needs return type (`@typescript-eslint/explicit-function-return-type`)
 function method3() {
+    list.push(6);
 }
 method3();
+
+// Needs accessors (`@typescript-eslint/explicit-member-accessibility`)
+class Goat {
+    name: string;
+    sound: unknown;
+}
+const goat: Goat = new Goat();
+goat.name = 'Bert';
+goat.sound = 'BLÖÖÖK';
+
+// Variable needs type definition (`@typescript-eslint/typedef`)
+const str = 'Hello World!';
+method1(str);
+
+// Properties need type definition (`@typescript-eslint/typedef`)
+interface Human {
+    name;
+    age;
+}
+
+// Member variables need type definition (`@typescript-eslint/typedef`)
+class User implements Human {
+    public name;
+    public age;
+    public height;
+}
+const user: User = new User();
+user.name = 'Klaus';
+user.age = 50;
+user.height = 180;
